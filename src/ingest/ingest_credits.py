5de4c6c30bf09345ccdec6cb5824f40f512d535c
shoti411 <lcposhoti411@gmail.com>
2026-05-07 16:06:15 -0400

    added repairing the json functionality

--- src/ingest/ingest_credits.py
+++ src/ingest/ingest_credits.py
@@ -5,6 +5,7 @@ from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.functions import col, when, count, avg, max as spark_max, sum as spark_sum, upper, lower, udf
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, BooleanType, TimestampType
 from ftfy import fix_text
+from json_repair import repair_json
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -31,8 +32,11 @@ credits_df.printSchema()
 credits_df.show()
 
 # this is for fixing mojibake in every string
-fix_text_udf = udf(lambda s: fix_text(s) if s is not None else None, StringType())
-fix_empty_list = udf(lambda s: "[]" if (s is None or s is "") else s)
+fix_text_udf = udf(lambda s: repair_json(fix_text(s)) if s is not None else None, StringType())
+fix_empty_list = udf(lambda s: "[]" if (s is None or s == "") else s)
+
+# fix_json_udf = udf(lambda s: s.apply())
+
 # replace null values in cast and crew as []
 cleaned_data = credits_df \
     .withColumn("id", when(credits_df["id"] <= 0, -1).otherwise(credits_df["id"])) \
@@ -43,14 +47,16 @@ cleaned_data = credits_df \
     .dropna(subset = ["id"]) \
     .dropDuplicates(subset = ['id']) 
 
-    # .withColumn("cast", fix_empty_list("cast")) \
-    # .withColumn("crew", fix_empty_list("crew")) \
-
 cleaned_data.write.parquet("src/silver/credits")
 
-print("cleaned up nulls and strings (not JSON yet)")
+print("cleaned up nulls and strings")
     
-        
+
+
+# to clean up json, we need to use the json loads func
+
+# replace every single quotes with double quotes
+
 
     # replace all the values within that have a negative id (for anything else, don't do anything)
     
@@ -58,7 +64,6 @@ print("cleaned up nulls and strings (not JSON yet)")
 
 
 # getting rid of duplicates in credits only based on the id
-#CleanData.remove_duplicates(credits_df, ['id'])
 
 
 
